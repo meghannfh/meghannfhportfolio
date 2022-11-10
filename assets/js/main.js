2d513f8c37bc62e9484cbb658b258d7effc432a4
meghann <meghannfh@gmail.com>
2022-11-10 13:06:20 -0600

    changes styles for accessibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,26 +89,46 @@ const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'fa-sun'
 
-// Previously selected topic (if user selected)
+// Previously selected version (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-sun' : 'fa-moon'
 
-// We validate if the user previously chose a topic
+// We validate if the user previously chose a light or dark version
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'fa-sun' ? 'add' : 'remove'](iconTheme)
+    if(selectedTheme === 'dark'){
+        document.body.classList.add('dark-theme')
+        themeButton.classList.add('fa-sun')
+        themeButton.classList.remove('fa-moon')
+    }else{
+        document.body.classList.remove('dark-theme')
+        themeButton.classList.add('fa-moon')
+        themeButton.classList.remove('fa-sun')
+    }
+
+//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+//   console.log(selectedTheme)
+//   console.log(selectedIcon)
+//   themeButton.classList[selectedIcon === 'fa-sun' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
+    console.log(selectedTheme)
+    console.log(selectedIcon)
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
+    if(document.body.classList.contains(darkTheme)){
+        themeButton.classList.add(iconTheme)
+        themeButton.classList.remove('fa-moon')
+    } else {
+        themeButton.classList.remove(iconTheme)
+        themeButton.classList.add('fa-moon')
+    }
+
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
